9b220106e17515adf3c30ba8bfd1e50058d26d58
Solinur <github@my-eg0.de>
2019-08-12 00:04:08 +0200

    API Bump, update Libs, release

--- Untaunted/Untaunted.lua
+++ Untaunted/Untaunted.lua
@@ -14,7 +14,7 @@ local AbilityCopies = {}
 Untaunted = Untaunted or {}
 local Untaunted = Untaunted
 Untaunted.name 		= "Untaunted"
-Untaunted.version 	= "0.2.19"
+Untaunted.version 	= "0.2.20"
 
 local function Print(message, ...)
 	if Untaunted.debug==false then return end
