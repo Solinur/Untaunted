bc829d711d7c346b87e090f12a9174264ce7b7b0
Solinur <github@my-eg0.de>
2020-06-07 21:18:41 +0200

    release

--- Untaunted/Untaunted.lua
+++ Untaunted/Untaunted.lua
@@ -14,7 +14,7 @@ local AbilityCopies = {}
 Untaunted = Untaunted or {}
 local Untaunted = Untaunted
 Untaunted.name 		= "Untaunted"
-Untaunted.version 	= "0.2.28"
+Untaunted.version 	= "0.2.29"
 
 local function Print(message, ...)
 	if Untaunted.debug==false then return end
