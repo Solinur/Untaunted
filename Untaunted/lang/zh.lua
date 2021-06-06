fbe4d6d6e5e0f129b080bc869ddeba9a18da9caa
Juijote <22154581+Juijote@users.noreply.github.com>
2021-06-06 10:28:50 +0800

    add chinese translation

---
+++ Untaunted/lang/zh.lua
@@ -0,0 +1,25 @@
+-- Menu --
+
+SafeAddString(SI_UNTAUNTED_LANG, "zh", 1)
+SafeAddString(SI_UNTAUNTED_FONT, "$(MEDIUM_FONT)", 1)
+
+SafeAddString(SI_UNTAUNTED_MENU_AW_NAME, "使用全账户设置",  1)	-- "Use Accountwide Settings"
+SafeAddString(SI_UNTAUNTED_MENU_AW_NAME_TOOLTIP, "如果选择了这个账户上的所有角色都将有相同的设置。",  1)	-- "If chosen all characters on this account will have the same Settings"
+SafeAddString(SI_UNTAUNTED_MENU_MOVE_BUTTON, "移动", 1)
+SafeAddString(SI_UNTAUNTED_MENU_MOVE_BUTTON_TOOLTIP, "移动嘲讽框", 1)
+SafeAddString(SI_UNTAUNTED_MENU_WINDOW_WIDTH, "宽度", 1)
+SafeAddString(SI_UNTAUNTED_MENU_WINDOW_WIDTH_TOOLTIP, "设置嘲讽框的宽度", 1)
+SafeAddString(SI_UNTAUNTED_MENU_WINDOW_HEIGHT, "高度", 1)
+SafeAddString(SI_UNTAUNTED_MENU_WINDOW_HEIGHT_TOOLTIP, "设置嘲讽框的高度", 1)
+SafeAddString(SI_UNTAUNTED_MENU_GROWTH_DIRECTION, "添加嘲讽框", 1)
+SafeAddString(SI_UNTAUNTED_MENU_GROWTH_DIRECTION_TOOLTIP, "当你选择它时，它就会显示一个新的嘲讽框。", 1)
+SafeAddString(SI_UNTAUNTED_MENU_MAX_BARS, "最大行数",  1)	-- "Maximum Rows"
+SafeAddString(SI_UNTAUNTED_MENU_MAX_BARS_TOOLTIP, "设置在框中显示的最大行数",  1)	-- "Maximum number of rows to show"
+SafeAddString(SI_UNTAUNTED_MENU_BAR_DIRECTION, "反转倒计时栏", 1)
+SafeAddString(SI_UNTAUNTED_MENU_BAR_DIRECTION_TOOLTIP, "当选择时，倒计时栏将向右减少。", 1)
+SafeAddString(SI_UNTAUNTED_MENU_SHOWMARKER, "显示敌人的标记", 1)
+SafeAddString(SI_UNTAUNTED_MENU_SHOWMARKER_TOOLTIP, "在战斗状态下，你可以在敌人的头顶上显示或设置一个类似小组标记的标记。", 1)
+SafeAddString(SI_UNTAUNTED_MENU_TRACKONLYPLAYER, "仅限玩家效果",  1)	-- "Only Player Effects"
+SafeAddString(SI_UNTAUNTED_MENU_TRACKONLYPLAYER_TOOLTIP, "如果你想只监控玩家施放的buff，请选择此项。",  1)	-- "Select to track only Player Effects"
+SafeAddString(SI_UNTAUNTED_MENU_TRACK, "%s 监控",  1)	-- e.g. "Track Taunt"
+SafeAddString(SI_UNTAUNTED_MENU_TRACK_TOOLTIP, "%s 选择监控",  1)	-- e.g. "Select to track Taunts"
